69295a154c3e383ab36d8e26d630b4aa518b1e9d
Mohammad Hassan <95434322+CriDeZ@users.noreply.github.com>
2023-02-12 14:02:57 +0200

    The Second example to use varuables and print it to screen

--- firstapp/src/App.jsx
+++ firstapp/src/App.jsx
@@ -4,10 +4,17 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const data = ["hello","world"];
+  const number1 = 5;
+  const number2 =6;
+  const string = "I love React!"
   return (
+    
     <div>
      <a href="#Clickme">Click Me</a>
+     <p>{data[0].charAt(0).toUpperCase()+data[0].slice(1)+ " "+data[1].charAt(0).toUpperCase()+data[1].slice(1)}</p>
+    <p>{number1}+{number2}={number1+number2}</p>
+    <p>{string + " " + string.length} </p>
     </div>
   )
 }
